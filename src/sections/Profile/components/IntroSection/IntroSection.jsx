47efb0ed0912c189995892627693815a3f5de50b
RusselxD <russelcabigquez8@gmail.com>
2025-03-10 02:50:15 +0800

    goodnight

--- src/sections/Profile/components/IntroSection/IntroSection.jsx
+++ src/sections/Profile/components/IntroSection/IntroSection.jsx
@@ -7,13 +7,13 @@ const IntroSection = () => {
         <div className="lg:-mt-32 w-full flex justify-center items-center">
             <div className="h-fit">
                 <h2 className="lg:text-3xl xl:text-4xl font-semibold lg:mb-3 xl:mb-5 text-primary">Hi,</h2>
-                <h1 className="font-bold lg:text-4xl xl:text-5xl lg:mb-3 xl:mb-5">
+                <h1 className="font-bold lg:text-[2.50rem] xl:text-5xl lg:mb-3 xl:mb-5">
                     I'm <span className="text-[rgba(10,207,245,255)]">Russel</span>
                 </h1>
-                <h1 className="lg:leading-10 font-bold lg:text-3xl xl:text-4xl lg:mb-4">
+                <h1 className="lg:leading-10 font-bold lg:text-[2rem] xl:text-4xl lg:mb-4">
                     Programmer &amp; Web Developer
                 </h1>
-                <p className="text-slate-700 md:text-base xl:text-lg mb-6 lg:leading-10 xl:leading-[2.7rem]">
+                <p className="text-slate-700 lg:text-[1rem] xl:text-lg mb-6 lg:leading-10 xl:leading-[2.7rem]">
                     I’m an IT student passionate about programming and building
                     functional, user-friendly applications. I love exploring new
                     technologies and constantly improving my ability to create
--- src/sections/Profile/components/Navbar.jsx
+++ src/sections/Profile/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/Logo.svg'
 
 const Navbar = () => {
     return (
-     <div className="translate-y-7 h-20 w-full flex justify-between items-center border">
+     <div className="translate-y-7 h-20 w-full flex justify-between items-center">
           <div className="flex justify-center items-center">
                <img src={logo} className="h-10 2-10 mr-2"/>
                <h1 className="text-xl font-semibold">Russel</h1>
