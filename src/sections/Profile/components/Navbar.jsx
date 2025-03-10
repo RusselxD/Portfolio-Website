9d17bee3d9bdf4c6374e2e50f9599b97f6fa10da
RusselxD <russelcabigquez8@gmail.com>
2025-03-11 00:45:44 +0800

    added an icon

--- src/sections/Profile/components/Navbar.jsx
+++ src/sections/Profile/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import logo from '../../../assets/Logo.svg'
+import logo from '/russel.svg'
 
 const Navbar = () => {
     return (
      <div className="translate-y-7 h-20 w-full flex justify-between items-center">
           <div className="flex justify-center items-center">
-               <img src={logo} className="h-10 2-10 mr-2"/>
-               <h1 className="text-xl font-semibold">Russel</h1>
+               <img src={logo} className="h-10 md:h-12 mr-2"/>
+               <h1 className="text-xl md:text-2xl font-semibold">Russel</h1>
           </div>
           <div></div>
      </div>
