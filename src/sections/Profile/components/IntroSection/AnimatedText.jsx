94e17f4d313a3e9535178212b624d4d9aff7d084
RusselxD <russelcabigquez8@gmail.com>
2025-03-11 20:49:24 +0800

    doneee????

--- src/sections/Profile/components/IntroSection/AnimatedText.jsx
+++ src/sections/Profile/components/IntroSection/AnimatedText.jsx
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from "react";
 
+const descriptions = ["Programmer", "Web Developer"];
+
 const test = "Programmer";
+
+function delay() {
+    return new Promise((resolve) => setTimeout(resolve, 500));
+}
 
 const AnimatedText = () => {
     const [text, setText] = useState("");
 
+    const [speed, setSpeed] = useState(120);
     const [isForward, setIsForward] = useState(true);
+    const [outerIndex, setOuterIndex] = useState(0);
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
-        const timeOut = setTimeout(() => {
+        const timeOut = setTimeout(async () => {
             if (isForward) {
-                if (index > test.length) {
+                if (index > descriptions[outerIndex].length) {
+                    await delay();
+                    setSpeed(50);
                     setIsForward(false);
                 } else {
-                    setText(test.slice(0, index));
+                    setText(descriptions[outerIndex].slice(0, index));
                     setIndex(index + 1);
                 }
             } else {
                 if (index <= 0) {
+                    setSpeed(120);
                     setIsForward(true);
+                    setOuterIndex((outerIndex + 1) % descriptions.length);
                 } else {
-                    setText(test.slice(0, index - 1));
+                    setText(descriptions[outerIndex].slice(0, index - 1));
                     setIndex(index - 1);
                 }
             }
-        }, 150);
+        }, speed);
 
         return () => clearTimeout(timeOut);
     }, [index, isForward]);
 
     return (
         <h1 className="lg:leading-10 font-bold text-2xl sm:text-3xl lg:text-[2rem] xl:text-4xl mb-4 sm:mb-5 md:mb-6 lg:mb-4">
-            {text}
+            {text}&nbsp;
         </h1>
     );
 };
